b51a97813b91cdc8ea862206e8357ac1db156dd2
Aayushi Garg <aayushi.garg@synapse.com>
2018-10-06 20:23:38 +0530

    Navbar Componenet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
-
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
 export default class NavBar extends Component {
   render () {
     return (
-      <h1>Navbar</h1>
+    <Navbar fixedTop dark expand="md" scrolling inverse collapseOnSelect>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a href="#home">CODUINO</a>
+            </Navbar.Brand>
+        </Navbar.Header>
+        <Nav pullRight>
+        <NavItem eventKey={1} href="#">
+        Home
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+        Projects
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+        Team
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+        Donate Us
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+        Wiki
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+        Get Involved
+        </NavItem>
+        </Nav>
+    </Navbar>
     )
   }
 }
